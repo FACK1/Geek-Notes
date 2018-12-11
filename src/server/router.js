f121d591c947e7ccf816e19a6db26958ae1c9b69
Amin <ayman201266@hotmail.com>
2018-12-11 14:18:20 +0200

    create router.js file #13

---
+++ src/server/router.js
@@ -0,0 +1,23 @@
+//-- Import Modules
+const fs = require('fs')
+const path = require('path');
+const handlers = require('./handlers.js');
+
+// Create Router
+const router = (request, response)=> {
+
+// Home Endpoint
+      if (request.url === '/') {
+            handlers.homeHandler(request,response)
+        }
+// Public Side Endpoint
+      else if (request.url.split('.')[1]) {
+            handlers.publicHandler(request,response)
+        }
+// Error Endpoint
+      else {
+            handlers.errorHandler(request,response)
+        }
+
+    };
+module.exports = router
