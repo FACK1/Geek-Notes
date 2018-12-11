65b39bf48816554832430f81748476fce4de7c35
alMutazBeAllah <alMutazBeAllah@gmail.com>
2018-12-11 13:56:53 +0200

    create users and notes tables #14

---
+++ src/database/db_build.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, notes CASCADE;
+
+CREATE TABLE users (
+     id         SERIAL PRIMARY KEY,
+     name       VARCHAR(255) NOT NULL,
+     user_name  VARCHAR(255) NOT NULL,
+     password   TEXT NOT NULL
+);
+
+CREATE TABLE notes (
+     id         SERIAL PRIMARY KEY,
+     user_id    INT REFERENCES users,
+     content    TEXT NOT NULL
+);
+
+COMMIT;
