dc7d4defbab6a3af918c22fb27ad0de0cdc8d423
Amin <ayman201266@hotmail.com>
2018-12-12 14:19:13 +0200

    eslint fix the errors #17

--- src/queries/setData.js
+++ src/queries/setData.js
@@ -1,13 +1,13 @@
 const dbConnection = require('../database/db_connection');
 
 const setUser = (name, username, password, cb) => {
-  const setUserQuery = 'INSERT INTO users (name, username, password) VALUES ($1, $2, $3);';
-  dbConnection.query(setUserQuery, [name, username, password], (error) => {
-    if (error) {
-      cb(error);
-    } else {
-      cb(null);
-    }
-  });
+  const setUserQuery = "INSERT INTO users (name, username, password) VALUES ($1, $2, $3);"
+  dbConnection.query(setUserQuery,[name, username, password],(error) => {
+  if (error){
+    cb(error);
+  } else {
+    cb(null);
+  }
+});
 };
-module.exports = { setUser };
+module.exports = {setUser};
