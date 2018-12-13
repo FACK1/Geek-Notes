83dafb7e0cde11ac161bf5a2a6d562b13f11ab47
alMutazBeAllah <alMutazBeAllah@gmail.com>
2018-12-13 11:10:27 +0200

    add getUserByUsername function #32

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,15 +1,15 @@
 const dbConnection = require('../database/db_connection');
 
-const getUserPasswordByUsername = (username, cb) => {
+const getUserByUsername = (username, cb) => {
     const getPasswordQuery = "SELECT id, password FROM users WHERE username = $1;";
     dbConnection.query(getPasswordQuery, [username], (err, result) => {
         if(err) {
             cb(err);
         } else {
             console.log(result);
-            cb(null, result);
+            cb(null, result.rows[0]);
         }
     });
 };
 
-module.exports = {getUserPasswordByUsername};
+module.exports = {getUserByUsername};
